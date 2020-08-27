ee9800697a49b1082e79f375913019447738bbdd
Kevin <your.email@address.com>
2020-08-27 15:55:33 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+let particleArray;
+
+//create constructor function
+function Particle(x, y, directionX, directionY, size, color) {
+  this.x = x;
+  this.y = y;
+  this.directionX = directionX;
+  this.directionY = directionY;
+  this.size = size;
+  this.color = color;
+}
+//add draw method to particle prototype
+Particle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+  ctx.fillStyle = this.color;
+  ctx.fill();
+};
+// const particle1 = new Particle(100,100,1,1,20, "white");
+// particle1.draw();
+
+//update
+Particle.prototype.update = function () {
+  if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+    this.directionX = -this.directionX;
+  }
+  if (this.y + this.size > canvas.height || this.y - this.size < 0) {
+    this.directionY = -this.directionY;
+  }
+  this.x += this.directionX;
+  this.y += this.directionY;
+  this.draw();
+};
+//create particle particleArray
+function init() {
+  particleArray = [];
+  for (let i = 0; i < 120; i++) {
+    let size = Math.random() * 65;
+    let x = Math.random() * (innerWidth - size * 2);
+    let y = Math.random() * (innerHeight - size * 2);
+    let directionX = Math.random() * 0.4 - 0.2;
+    let directionY = Math.random() * 0.4 - 0.2;
+    //let color = "white";
+    const colors = [
+      "rgba(292,292,292)",
+      "rgba(255, 255, 255, 0.3)",
+      "rgba(173, 216, 230, 0.8)",
+      "rgba(211, 211, 211, 0.8)",
+    ];
+    let color = colors[Math.floor(Math.random() * colors.length)];
+    particleArray.push(new Particle(x, y, directionX, directionY, size, color));
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+  }
+}
+
+init();
+animate();
+
+window.addEventListener("resize", function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  init();
+});
